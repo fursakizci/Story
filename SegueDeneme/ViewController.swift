a6b1f2cfa76bf0a215f39e012cabe44720c8ca18
furkan SAKIZCI <fursakizci@gmail.com>
2019-04-28 13:59:10 +0300

    Initial Commit

---
+++ SegueDeneme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueDeneme
+//
+//  Created by Furkan sakızcı on 28.04.2019.
+//  Copyright © 2019 Furkan sakızcı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
